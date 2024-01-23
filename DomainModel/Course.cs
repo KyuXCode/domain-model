04af7b825afcb81cbd912f22e05605faaa28efec
Kevin-Yu <kai38547@gmail.com>
2024-01-23 13:37:51 -0500

    Add course class

---
+++ DomainModel/Course.cs
@@ -0,0 +1,46 @@
+﻿namespace DomainModel;
+
+public class Course
+{ 
+    int id;
+    int courseNumber;
+    string courseName;
+
+    List<Student> students;
+    List<Assignment> assignments;
+
+    Instructor instructor;
+    
+    public Course() 
+    {
+        this.students = new List<Student>();
+        this.assignments = new List<Assignment>();
+        this.instructor = new Instructor;
+    }
+    public List<Assignment> getAssignments()
+    {
+        return this.assignments;
+    }
+
+    public List<Student> getStudents() 
+    {
+        return this.students;
+    }
+    public int getId() 
+    {
+        return this.id;
+    }
+    public int getCourseNumber() 
+    {
+        return this.courseNumber;
+    }
+    public string getCourseName() 
+    {
+        return this.courseName;
+    }
+
+    public Instructor getInstructor()
+    {
+        return this.instructor;
+    }
+}
