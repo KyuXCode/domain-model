e2d7d7932a24860be580349b1483ceb7f67d9471
Kevin-Yu <kai38547@gmail.com>
2024-01-23 13:39:29 -0500

    Add instructor class

---
+++ DomainModel/Instructor.cs
@@ -0,0 +1,29 @@
+namespace DomainModel;
+
+public class Instructor
+{
+    
+        int id;
+        string firstName;
+        string lastName;
+        string email;
+
+        public int getId() 
+        {
+            return this.id;
+        }
+        public string getFirstName() 
+        {
+            return this.firstName;
+        }
+        public string getLastName() 
+        {
+            return this.lastName;
+        }
+        public string getEmail() 
+        {
+            return this.email;
+        }
+
+    
+}
